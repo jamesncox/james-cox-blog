2877fba44827a8aa74f24470dbf10b71056c965c
jamesncox <jamesncox@yahoo.com>
2021-05-24 17:36:18 -0500

    Update first-post.js with link back

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center min-h-screen py-2">
       <Head>
         <title>James' Blog</title>
         <link rel="icon" href="/favicon.ico/?v=2" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <main className="flex flex-col items-center w-full flex-1 px-20 text-center">
         <h1 className="font-mono font-bold text-xl">
           Read{" "}
           <Link href="/posts/first-post">
-            <a className="text-blue-600 ">this page!</a>
+            <a className="text-blue-600">this page!</a>
           </Link>
         </h1>
       </main>
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function FirstPost() {
-  return <h1>First Post</h1>;
+  return (
+    <div className="flex flex-col items-center w-full flex-1 px-20 text-center">
+      <h1 className="text-xl font-bold text-green-600 mt-5">First Post</h1>
+      <h2>
+        <Link href="/">
+          <a className="font-mono font-bold text-xl text-blue-600">
+            Back to home
+          </a>
+        </Link>
+      </h2>
+    </div>
+  );
 }
