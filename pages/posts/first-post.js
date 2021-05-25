5aa84b114aff9294b66f949b1657d08ecd053028
jamesncox <jamesncox@yahoo.com>
2021-05-25 08:31:37 -0500

    Import layout as Layout and wrap fist-post in Layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -17,6 +18,6 @@ export default function FirstPost() {
           </Link>
         </h2>
       </div>
-    </>
+    </Layout>
   );
 }
